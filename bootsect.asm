c03d3da6a74563502a1c03132be3b116972d4c64
hooss-only <hoossonly@gmail.com>
2025-08-05 22:18:50 +0900

    feat: read disk

--- bootsect.asm
+++ bootsect.asm
@@ -1,23 +1,26 @@
 [org 0x7c00]
+mov bp, 0x8000
+mov sp, bp
 
-mov bx, HELLO
-call print
-call print_nl
+mov bx, 0x9000
+mov dh, 2 ; sector
+call disk_load
 
-mov bx, BYE
-call print
-call print_nl
+mov dx, [0x9000]
+call print_hex
+
+mov dx, [0x9000 + 512]
+call print_hex
 
 jmp $
 
 %include "bootsect/print.asm"
-
-HELLO:
-  db 'Hello, world', 0
-
-BYE:
-  db 'Good Bye', 0
+%include "bootsect/print_hex.asm"
+%include "bootsect/disk.asm"
 
 times 510-($-$$) db 0
 dw 0xaa55
 
+
+times 256 dw 0xdada
+times 256 dw 0xface
