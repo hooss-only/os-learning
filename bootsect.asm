b9c16a098ee2bebe54a240b0705b0f43ce6a1588
hooss-only <hoossonly@gmail.com>
2025-08-06 15:07:45 +0900

    feat: 32-bits protected mode

--- bootsect.asm
+++ bootsect.asm
@@ -1,26 +1,26 @@
 [org 0x7c00]
-mov bp, 0x8000
-mov sp, bp
+  mov bp, 0x9000
+  mov sp, bp
 
-mov bx, 0x9000
-mov dh, 2 ; sector
-call disk_load
+  mov bx, MSG_REAL_MODE
+  call print
 
-mov dx, [0x9000]
-call print_hex
-
-mov dx, [0x9000 + 512]
-call print_hex
-
-jmp $
+  call switch_to_pm
+  jmp $
 
 %include "bootsect/print.asm"
-%include "bootsect/print_hex.asm"
-%include "bootsect/disk.asm"
+%include "bootsect/32bit_print.asm"
+%include "bootsect/gdt.asm"
+%include "bootsect/switch_pm.asm"
+
+[bits 32]
+BEGIN_PM:
+  mov ebx, MSG_PROT_MODE
+  call print_string_pm
+  jmp $
+
+MSG_REAL_MODE db "Started in 16-bits real mode.", 0
+MSG_PROT_MODE db "Loaded 32-bits protected mode.", 0
 
 times 510-($-$$) db 0
 dw 0xaa55
-
-
-times 256 dw 0xdada
-times 256 dw 0xface
