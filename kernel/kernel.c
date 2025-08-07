f828e43068874fb3a5dbb6674a49cff03a700401
hooss-only <hoossonly@gmail.com>
2025-08-07 18:02:56 +0900

    feat: screen

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,18 +1,9 @@
-#include "../drivers/ports.h"
+#include "../drivers/screen.h"
 
 int main() {
-  port_byte_out(0x3d4, 14);
-  int position = port_byte_in(0x3d5);
-  position = position << 8;
-
-  port_byte_out(0x3d4, 15);
-  position += port_byte_in(0x3d5);
-
-  int offset_from_vga = position * 2;
-
-  char* vga = (char*) 0xb8000;
-  vga[offset_from_vga] = 'X';
-  vga[offset_from_vga+1] = 0x0f;
+  clear_screen();
+  kprint("os-learning project\n");
+  kprint("Kernel Loaded.");
 
   return 0;
 }
